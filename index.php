350727e2ee917fd3791f9b1522616ca1459fd70f
Bakhtiyar <190107023@stu.sdu.edu.kz>
2021-03-23 23:33:55 +0600

    5

--- index.php
+++ index.php
@@ -23,8 +23,46 @@ try {
     });
     $bot->sendMessage("410782452", "Success");
 
+    botMessage($token, "Success", "410782452");
+
     $bot->run();
 
 } catch (\TelegramBot\Api\Exception $e) {
     $e->getMessage();
+}
+
+function botMessage($token, $data, $chat_id=''){
+    $txt = $this->ArrToStr($data);
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    curl_exec($curl);
+
+    curl_close($curl);
+
+}
+
+function ArrToStr($data, $lev=0) {
+    $str = [];
+    $tab = str_repeat("%20", $lev * 4);
+    if(is_array($data)) {
+        foreach ($data as $k => $v) {
+            $str[$k] = $tab.str_repeat("-", $lev == 1).$k.":%20".ArrToStr($v, $lev+1);
+        }
+        $str = (($lev==0)?"":"%0A").implode(str_repeat("%0A", 2 - ($lev!=0)), $str);
+    }
+    else {
+        $str = trim(strip_tags($data));
+    }
+    return $str;
 }
