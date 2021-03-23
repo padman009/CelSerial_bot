eb64f73a83bd81781b16c1cd8d83e1c1ef604473
Bakhtiyar <190107023@stu.sdu.edu.kz>
2021-03-23 23:48:50 +0600

    8

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9fa5bff39faaaa02d2f42a1a40e165110a432d5f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fa5bff39faaaa02d2f42a1a40e165110a432d5f',
+    ),
+    'telegram-bot/api' => 
+    array (
+      'pretty_version' => 'v2.3.22',
+      'version' => '2.3.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9a69f82121a5ef0e396c0c3a8be86caefd1376a8',
+    ),
+  ),
+);
