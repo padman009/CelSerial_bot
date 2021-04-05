00765f15661b965de6499c418aac74493b60a8b7
Bakhtiyar <190107023@stu.sdu.edu.kz>
2021-04-05 20:02:49 +0600

    Добавление шоу в список и отмена команды

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use TelegramBot\Api\BotApi;
 use TelegramBot\Api\Client;
+use TelegramBot\Api\Types\ReplyKeyboardMarkup;
 
 require_once "vendor/autoload.php";
 include_once "main.php";
@@ -28,11 +29,6 @@ try {
     });
 
     $botClient->command('addshow', function ($message) use ($bot) {
-        $text = $message->getText();
-//        $user_input["episodes"] = getEpisodesFromUserText($text);
-//        $user_input["chat_id"] = $message->getChat()->getId();
-
-//        $answer = storeUserInput($user_input) ? "Success added" : "Fail in adding";
         $answer =
             "Отправьте название шоу (так как и на сайте) и озвучки в формате *название*(*озвучка*)\n
             Вы можете отправить несколько шоу в одном сообщении написав по одному шоу в строку\n
@@ -45,12 +41,49 @@ try {
         $bot->sendMessage($message->getChat()->getId(), $answer);
     });
 
-    $data = json_decode(file_get_contents("php://input"), true);
 
-//    $bot->sendMessage($data["message"]["from"]["id"], $data["message"]["text"]);
+
+    $botClient->command('cancel', function ($message) use ($bot) {
+        $answer = "Command canceled";
+        $statuses = json_decode(file_get_contents("status.json"), true);
+        unset($statuses[$message->getChat()->getId()]);
+        file_put_contents("status.json", json_encode($statuses));
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+
+//
+//    echo json_encode($data);
+//
+//    if($data->send){
+//    $replyKeyboard = new \TelegramBot\Api\Types\ReplyKeyboardRemove();
+
+//    }
+
+    addShowCheck($bot);
+    $botClient->run();
+
+    $sendKeyboard =  function ($id, $text,Array $keyboard, $one_time = true, $resize_keyboard = true) use($bot){
+        $replyKeyboard = new ReplyKeyboardMarkup($keyboard, $one_time, $resize_keyboard);
+        $bot->sendMessage($id,$text, null, false, null, $replyKeyboard);
+    };
+
+//    $sendKeyboard("410782452", "Choose",[["4", "3"]]);
+
+} catch (\TelegramBot\Api\Exception $e) {
+    echo $e->getMessage();
+    $bot->sendMessage("410782452", json_encode($e->getMessage()));
+}
+
+function addShowCheck($bot)
+{
+    $data = json_decode(file_get_contents("php://input"), true);
+    if(isset($data["message"]["entities"][0]["type"]) && $data["message"]["entities"][0]["type"] == "bot_command"){
+        return;
+    }
 
     $statuses = json_decode(file_get_contents("status.json"), true);
-    if(isset($statuses[$data["message"]["from"]["id"]]) && $statuses[$data["message"]["from"]["id"]] == "addshow"){
+    $status = isset($statuses[$data["message"]["from"]["id"]]) ? $statuses[$data["message"]["from"]["id"]] : "";
+    if($status == "addshow"){
         $new_shows["chat_id"] = $data["message"]["from"]["id"];
         $new_shows["episodes"] = getEpisodesFromUserText($data["message"]["text"]);
         $response = storeUserInput($new_shows);
@@ -59,18 +92,7 @@ try {
             $bot->sendMessage("410782452", json_encode($response));
             $bot->sendMessage($data["message"]["from"]["id"], "Adding show failed");
         }else{
-            $bot->sendMessage($data["message"]["from"]["id"], "Шоу успешно добавлено!\nПроверьте командой /addshow");
+            $bot->sendMessage($data["message"]["from"]["id"], "Шоу успешно добавлено!\nПроверьте командой /listshow");
         }
     }
-//
-//    echo json_encode($data);
-//
-//    if($data->send){
-//        $bot->sendMessage("410782452",$data->text);
-//    }
-
-    $botClient->run();
-
-} catch (\TelegramBot\Api\Exception $e) {
-
 }
